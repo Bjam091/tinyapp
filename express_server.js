7543a19595e037640288170160667d339275d45a
Brandon <brandonjamiller@hotmail.com>
2019-09-30 21:27:28 +0000

    express_server.js added urls/short path

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.LongURL };
+  res.render("urls_show", templateVars);
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
