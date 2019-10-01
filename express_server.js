5b6c1767006b464cc1890e8b1f8f789320b12759
Brandon <brandonjamiller@hotmail.com>
2019-10-01 23:03:33 +0000

    logout button implemented

--- express_server.js
+++ express_server.js
@@ -26,7 +26,7 @@ const generateRandomString = function() {
 app.set("view engine", "ejs");
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"], };
   res.render("urls_index", templateVars);
 });
 
@@ -37,11 +37,16 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"], 
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -68,6 +73,11 @@ app.listen(PORT, () => {
 
 app.post("/urls/signIn", (req,res) => {
   res.cookie("username", req.body.username)
+  res.redirect("/urls")
+})
+
+app.post("/urls/logout", (req,res) => {
+  res.clearCookie("username", req.body.username)
   res.redirect("/urls")
 })
 
