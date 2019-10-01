265ba95757e41290f414cb56e6ff58c50d57fe06
Brandon <brandonjamiller@hotmail.com>
2019-10-01 06:58:49 +0000

    express_server.js with edit button implemented

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
+app.post("/urls/:shortURL", (req,res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL/delete", (req,res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls") 
