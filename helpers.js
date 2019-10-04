297bbec23197ce60c4e36851cf8b02d5a9e1879e
Brandon <brandonjamiller@hotmail.com>
2019-10-04 00:41:43 +0000

    helpers.js with commented code

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+//gets the user infromation based on their email being compared to the database
+
 const getUserByEmail = function(email, users) {
   for (let uID in users) {
     if (users[uID].email === email) {
